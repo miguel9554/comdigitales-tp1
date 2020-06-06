a38ca822d3ab019062ef048433e4266104543bb4
matijaure <38586521+matijaure@users.noreply.github.com>
2020-06-05 23:18:51 -0300

    Add files via upload

---
+++ 2/PSK.py
@@ -0,0 +1,92 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import NormalDist
+import random
+
+class PSK:
+    def __init__(self, M: int, pulse_energy: float):
+        self.M = M
+        # Cantidades asociadas a la energía
+        self.pulse_energy = pulse_energy
+        self.average_energy = pulse_energy
+        self.bit_energy = pulse_energy/np.log2(M)
+        self.min_distance = 2*np.sqrt(np.pi**2*np.log2(self.M)*np.sin(np.pi/M)**2*self.bit_energy)
+        # Construimos los símbolos
+        # Representamos los símbolos con una matriz cuyas filas son las coordenadas de cada símbolo
+        # Es de MxN (M órden de la constelación, N dimensión)
+
+        self.symbols = np.matrix([[pulse_energy*np.cos(2*np.pi*(m-1)/M), pulse_energy*np.cos(2*np.pi*(m-1)/M)] \
+                for m in range(1,self.M+1)])
+        self.symbols_energy = np.matrix([ [x] for x in np.linalg.norm(self.symbols, axis=-1)**2])
+
+    def estimate_pe(self, SNRdb, iterations):
+
+        # Obtenemos N0
+        SNRveces = 10**(SNRdb/10)
+        N0 = self.bit_energy/SNRveces
+
+        # Envíamos símbolos al azar y les agregamos ruido, detectamos lo recibido
+        # y contamos los errores
+        errors = 0
+        for sent in random.choices(self.symbols, k=iterations):
+            noise = np.random.multivariate_normal(np.zeros(2), np.diag([N0/2, N0/2]), 1)
+            received = np.transpose(sent + noise)
+
+            # Detectamos
+            correlator_output = self.symbols*received
+            bias = N0/2*np.log(1/self.M)-self.symbols_energy/2
+            detected = self.symbols[np.argmax(correlator_output+bias)]
+            if not np.array_equal(sent, detected):
+                errors += 1
+
+        return errors/iterations
+
+    def montecarlo_pe_vs_snr(self, SNRmax, iterations):
+
+        SNRs = range(SNRmax+1)
+        pe_vec = []
+
+        for SNR in SNRs:
+            pe_vec.append(self.estimate_pe(SNR, iterations))
+
+        return SNRs, pe_vec
+
+    def theorical_pe_vs_snr(self, SNRmax):
+        
+        SNRs = list(range(SNRmax+1))
+        if self.M ==2:
+            return SNRs, [ 1*() for SNR in SNRs]
+            
+        return SNRs, [ 2*(1-1/np.sqrt(self.M))*Q(np.sqrt(3*np.log2(self.M)*10**(SNR/10)/(self.M-1))) for SNR in SNRs]
+
+def Q(q):
+
+    return 1 - NormalDist(mu=0, sigma=1).cdf(q)
+
+
+def main():
+
+    Nsamples = int(1e3)
+    SNRmax = 10
+    pulse_energy = 1
+    varios_M = [4,8,16]
+
+    for M in varios_M:
+        constelation = QAM(M, pulse_energy)
+        SNRs_estimacion, pe_estimada = constelation.montecarlo_pe_vs_snr(SNRmax, Nsamples)
+        SNRs_teoricas, pe_teorica = constelation.theorical_pe_vs_snr(SNRmax)
+
+        plt.plot(SNRs_teoricas, pe_teorica, label='Teórica M= %d' %M,color='r',zorder=1)
+        plt.scatter(SNRs_estimacion, pe_estimada, label='Estimación M = %d' %M,zorder=2)
+        
+    plt.title('Probabilidad de error para QAM')
+    plt.yscale('log')
+    plt.xlabel('SNR [dB]')
+    plt.ylabel('log(Probabilidad de error)')
+    plt.legend()
+    plt.grid()
+    plt.savefig('results/QAM.png')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
